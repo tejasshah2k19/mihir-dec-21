df762b4d0041df1e81b24d9b44b2e6d4c0bc1a65
Tejas <tejas14all@gmail.com>
2022-01-16 11:44:57 +0530

    single link list - display

--- singlelinklist.c
+++ singlelinklist.c
@@ -5,10 +5,8 @@ struct node{
     struct node *next;//??->4
 }*head=NULL,*last=NULL; //  link list is empty
 
-
-int main(){
-        int *p;
-        struct node *tmp;
+void addNode(){
+  struct node *tmp;
 
         //loop -> 5
         int num;
@@ -29,6 +27,75 @@ int main(){
             last = tmp; //last = last -> next;
 
         }
+
+
+}
+
+/*
+void display(){
+    struct node *p;
+    p = head;
+
+    abc:
+        printf("%d->",p->data);// 10 20 30
+        p=p->next ;
+
+    if( p != NULL){
+            goto abc;
+    }
+}
+*/
+
+
+void display(){
+    struct node *p;
+/*
+    for(p=head;p!=NULL;p=p->next){ //p++ p--
+        printf("%d->",p->data);// 10 20 30
+    }
+*/
+    p= head;
+
+    while(p!=NULL){
+        printf("%d-> ",p->data);
+        p=p->next;
+    }
+
+}
+
+void search(){
+
+}
+
+
+
+
+int main(){
+        int *p;
+        int choice;
+
+
+        while(1){
+            printf("\n0 for exit\n1 for add\n2 for display\n3 for search\nenter choice");
+            scanf("%d",&choice);
+
+            switch(choice){
+                    case 0:exit(0);
+
+                    case 1:
+                        addNode();
+                        break;
+
+                    case 2:
+                        display();
+                        break;
+
+                    case 3:
+                        search();
+                        break;
+            }
+        }
+
 
 
 
